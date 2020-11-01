447d3f7a718b60755362f798e0d57c51485dcdd9
tamdv <tamdv.neonstudio@gmail.com>
2020-11-01 20:57:51 +0700

    proxy

--- app/src/main/java/com/example/whats_app/ProfileActivity.java
+++ app/src/main/java/com/example/whats_app/ProfileActivity.java
@@ -3,11 +3,11 @@ package com.example.whats_app;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,25 +19,29 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private String receiverUserId;
+    private String receiverUserId, currentState, currentUserId;
     private CircleImageView userProfileImageView;
     private TextView userProfileName, userProfileStatus;
     private Button sendRequestMessage;
 
     private DatabaseReference userRef;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        mAuth = FirebaseAuth.getInstance();
         userRef = FirebaseDatabase.getInstance().getReference().child("Users");
         receiverUserId = getIntent().getExtras().get("visitUserId").toString();
 
+        currentUserId =mAuth.getCurrentUser().getUid();
         userProfileImageView = findViewById(R.id.visit_profile_image);
-        userProfileName = (TextView) findViewById(R.id.user_profile_name);
-        userProfileStatus = (TextView)findViewById(R.id.user_profile_status);
+        userProfileName = (TextView) findViewById(R.id.visit_profile_username);
+        userProfileStatus = (TextView) findViewById(R.id.visit_profile_status);
         sendRequestMessage = findViewById(R.id.send_request_button);
-
+ currentState = "new";
         RetrieveUserInfo();
     }
 
@@ -45,7 +49,7 @@ public class ProfileActivity extends AppCompatActivity {
         userRef.child(receiverUserId).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if((snapshot.exists()) && snapshot.hasChild("image")){
+                if ((snapshot.exists()) && snapshot.hasChild("image")) {
                     String userImage = snapshot.child("image").getValue().toString();
                     String userName = snapshot.child("name").getValue().toString();
                     String userStatus = snapshot.child("status").getValue().toString();
@@ -53,8 +57,9 @@ public class ProfileActivity extends AppCompatActivity {
                     Picasso.get().load(userImage).placeholder(R.drawable.profile_image).into(userProfileImageView);
                     userProfileName.setText(userName);
                     userProfileStatus.setText(userStatus);
-                }else {
-                        String userName = snapshot.child("name").getValue().toString();
+                    ManageChatRequest();
+                } else {
+                    String userName = snapshot.child("name").getValue().toString();
                     String userStatus = snapshot.child("status").getValue().toString();
 
                     userProfileName.setText(userName);
@@ -68,5 +73,8 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void ManageChatRequest() {
     }
 }
